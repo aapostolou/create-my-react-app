e2e15a9b251cf73234afa4bcbfbbe8a8f2c615d6
aapostolou <alexandros.apostolou@outlook.com>
2020-08-04 00:38:38 +0300

    master

--- index.js
+++ index.js
@@ -12,15 +12,20 @@ const unzipper = require("unzipper");
 
 const url = "https://github.com/aapostolou/React-Redux/archive/master.zip";
 
+const rootDir = path.join(__dirname, "..");
+
+console.log(rootDir);
+
 console.log();
 console.log("===========================");
+console.log();
 
 // Not eneough arguments
 if (process.argv.length < 3) {
   console.log(" Please specify 'app name'");
 } // Thats the spot !
 else if (process.argv.length < 4) {
-  const targetDir = path.join(__dirname, process.argv[2]);
+  const targetDir = path.join(rootDir, process.argv[2]);
 
   // Check if folder exists
   if (!fs.existsSync(targetDir)) {
@@ -28,19 +33,19 @@ else if (process.argv.length < 4) {
 
     (async () => {
       // Download files
-      await download(url, __dirname, { filename: "temp.zip" });
+      await download(url, rootDir, { filename: "temp.zip" });
 
       // Unzip them
-      await fs.createReadStream(`${__dirname}/temp.zip`).pipe(
-        unzipper.Extract({ path: `${__dirname}` }).on("finish", (data) => {
+      await fs.createReadStream(`${rootDir}/temp.zip`).pipe(
+        unzipper.Extract({ path: `${rootDir}` }).on("finish", (data) => {
           setTimeout(
             () =>
               // Rename to target dir name
-              fs.rename(`${__dirname}/React-Redux-master`, targetDir, (err) => {
+              fs.rename(`${rootDir}/React-Redux-master`, targetDir, (err) => {
                 if (err) throw err;
 
                 // Delete Zip
-                fs.unlink(`${__dirname}/temp.zip`, (err2) => {
+                fs.unlink(`${rootDir}/temp.zip`, (err2) => {
                   if (err2) throw err2;
                 });
               }),
@@ -58,5 +63,6 @@ else {
   console.log(process.argv.slice(2));
 }
 
+console.log();
 console.log("===========================");
 console.log();
